aa831418fad6016ae625d21579065f7ff5585c32
Krishna Sapare <johndoe@example.com>
2022-09-08 13:27:44 +0530

    First Commit

---
+++ src/main/java/com/example/test/demoapp/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.test.demoapp;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String helloWorld(String name){
+        return "Hello Works";
+    }
+}
